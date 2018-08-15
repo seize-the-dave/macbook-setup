3008c1888c5c97286a5f4f43ab26c0665616f2a6
Dave Grant <david@grant.org.uk>
2018-08-15 16:18:10 +0100

    Updated defaults

--- defaults.sh
+++ defaults.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash -x
 
-# Dock
+# Change dock defaults
 defaults delete com.apple.dock persistent-apps
-defaults delete com.apple.dock persistent-others
-defaults write com.apple.dock tilesize -int 40
+defaults write com.apple.dock tilesize -int 60
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock largesize -int 100
+defaults write com.apple.dock "minimize-to-application" -bool true
+defaults write com.apple.dock mineffect -string genie
 killall Dock
+
+# Add option to quit Finder
+defaults write com.apple.finder QuitMenuItem -bool true
+killall Finder
+
+# No screenshot shadows
+defaults write com.apple.screencapture disable-shadow -bool true
+killall SystemUIServer
